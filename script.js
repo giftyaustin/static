f8903fdef11ae7025aa056d94002d10427396f15
surendesaboina <surenkumar13579@gmail.com>
2023-02-21 00:51:39 +0530

    added animation to the skills button to display skills when the button is clicked

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ function dropthenav(){
         x.className="";
     }
 }
+
+function skillDropClicked(){
+    var x = document.getElementById("skilldrop");
+    var y = document.getElementById("skillsbuttonclicked");
+    if (x.className==="skill-display"){
+        x.className+= " skill-display-clicked";
+        y.className+= " item-s-clicked";
+    }
+    else{
+        x.className="skill-display";
+        y.className+= "item-s";
+
+    }
+}
