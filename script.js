6bf01377edb126d5b2c16da244ee9934b3186188
surendesaboina <surenkumar13579@gmail.com>
2023-03-04 12:24:44 +0530

    added extra content to skill bar

--- script.js
+++ script.js
@@ -11,13 +11,41 @@ function dropthenav(){
 function skillDropClicked(){
     var x = document.getElementById("skilldrop");
     var y = document.getElementById("skillsbuttonclicked");
+    var a = document.getElementById("ed-clicked");
+    var p = document.getElementById("educationClicked");
+    p.className="item-s";
+    a.className="skill-display";
     if (x.className==="skill-display"){
         x.className+= " skill-display-clicked";
         y.className+= " item-s-clicked";
+
     }
     else{
         x.className="skill-display";
         y.className+= "item-s";
 
     }
+}
+
+function educationClicked(){
+    var x= document.getElementById("ed-clicked");
+    var y = document.getElementById("skilldrop");
+    var p = document.getElementById("educationClicked");
+    if (x.className=="educationdisplay"){
+        x.className="skill-display";
+        p.className="item-s";
+        y.className="skill-display";
+        var z = document.getElementById("skillsbuttonclicked");
+        z.className+= "item-s";
+        return;
+    }
+    p.className+=" item-s-clicked";
+   
+    x.className="educationdisplay";
+    y.className="skill-display";
+    var z = document.getElementById("skillsbuttonclicked");
+    z.className+= "item-s";
+
+
+
 }
